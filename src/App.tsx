030266ba2ad6e13d3e46c197b8c84d236540a8a4
Dion Pimentel <dionpimentel260@gmail.com>
2023-09-24 23:58:54 -0700

    Updated react-scripts and added website placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Page under rewrite to React - Dion.
         </p>
         <a
           className="App-link"
